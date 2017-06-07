9a571c05610bb23f95f338c75982ddcc01bf8c5e
RAMYA14597 <ramyaerusan@gmail.com>
2017-06-07 12:25:04 +0530

    Create checkingnum.java

---
+++ checkingnum.java
@@ -0,0 +1,20 @@
+class checkingnum
+{
+public static void main(String args[])
+{
+int pcount=0,negcount=0;
+int number=Integer.parseInt(args[0]);
+if(num==0)
+{
+System.out.println("the number is ZERO");
+}
+else if(number>0)
+{
+System.out.println("the number is positive");
+}
+else
+{
+System.out.println(the number is negative");
+}
+}
+}
