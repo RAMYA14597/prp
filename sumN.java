572d6f798dc6a4422db1e036811e0b10f45a97b1
RAMYA14597 <ramyaerusan@gmail.com>
2017-06-08 14:02:25 +0530

    Create sumN.java

---
+++ sumN.java
@@ -0,0 +1,14 @@
+class sum
+{
+    public static void main(String args[])
+    {
+        int a=Integer.parseInt(args[0]);
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum=sum+i;
+        }
+        System.out.println(sum+"");
+        
+    }
+}
