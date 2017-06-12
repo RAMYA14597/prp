e88160f8ac92ecf1f6a4ac799f0882573de0de0b
RAMYA14597 <ramyaerusan@gmail.com>
2017-06-11 18:44:51 -0700

    Create multable.java

---
+++ multable.java
@@ -0,0 +1,12 @@
+class multable
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=i;i<=10;i++)
+{
+System.out.print(n+"*"+i+"="+num*i);
+}
+}
+}
