bf206e8d1431b51c284e7b775a608a6aa64894fb
RAMYA14597 <ramyaerusan@gmail.com>
2017-06-13 11:15:44 +0530

    Create ascii.java

---
+++ ascii.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ascii
+{
+    public static void main(String args[])
+    {Scanner sc=new Scanner(System.in);
+        for(int i=0;i<26;i++)
+        {
+            
+            System.out.println((char)('a'+i)+"="+('a'+i));
+        }
+        
+    }
+}
