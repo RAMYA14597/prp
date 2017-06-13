0ac8391dea9ac136a998ac534d334874711f9f6e
RAMYA14597 <ramyaerusan@gmail.com>
2017-06-13 11:54:51 +0530

    Create hcf.java

---
+++ hcf.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class hcf
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x,y,r;
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        x=n1;
+        y=n2;
+        while(n2>0)
+        {
+            r=n1%n2;
+            n1=n2;
+            n2=r;
+        }
+        int gcd=n1;
+        System.out.println(gcd+"");
+    }
+}
