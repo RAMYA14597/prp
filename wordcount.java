b2e8062e4e2dc7cf6fe1e932e17c235adb5c7a65
RAMYA14597 <ramyaerusan@gmail.com>
2017-07-05 10:16:57 -0700

    Create wordcount.java

---
+++ wordcount.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class wordcount {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+	    String s=sc.nextLine();
+	    String[] ss=s.split(" ");
+	    int count=0;
+	    for(String g:ss)
+	    {
+	    	count++;
+	    }
+	    System.out.println(count);
+	}
+
+}
