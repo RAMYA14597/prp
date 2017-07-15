4ed8656d6219884720503493c08ad4ad74ad8bef
RAMYA14597 <ramyaerusan@gmail.com>
2017-07-15 07:00:45 -0700

    Create numberrange.java

---
+++ numberrange.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class input {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int num=sc.nextInt();
+try{
+if(num<=100 && num>=0){
+	System.out.println(num);
+}
+else if(num<0){
+	throw new NumberFormatException();
+}
+else{
+	throw new Exception();
+}}
+catch(NumberFormatException ef){
+	System.out.println("java.lang.Exception:Error!!Entered limit is of Negative Range...");
+	System.out.println("Please Enter the valid input...!!!");
+	System.exit(0);
+}
+catch(Exception e)
+{
+System.out.println("java.lang.Exception:Error!!Entered limit is out of Range...");
+System.out.println("Please Enter the valid input...!!!");
+System.exit(0);
+}}}
