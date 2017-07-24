22b0e8686913d9d2719c9d143ce0010eb2fa36f1
RAMYA14597 <ramyaerusan@gmail.com>
2017-07-23 23:43:08 -0700

    Create new file

---
+++ decimaltobinary.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class decimal {
+
+	public static void main(String[] args) {
+		
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter the decimal number: ");
+			    int num=sc.nextInt();
+			    if(num<0)
+			    {
+			    System.out.println("enter the positive number");	
+			    }	
+			    else
+			    {
+			    	System.out.println("The corresponding binary of "+num+" is: ");
+			    	decimal1(num);
+			    	
+			    }
+	}
+	public static void decimal1(int num)
+	{
+		if(num<=1)
+		{
+			System.out.print(num);
+			return;
+			
+		}
+			int rem=num%2;
+			decimal1(num>>1);
+			System.out.print(rem);
+		
+		
+		
+	}
+
+}
