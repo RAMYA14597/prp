18d1a8f0ea2b17f713b2e70fe4518497afecab52
RAMYA14597 <ramyaerusan@gmail.com>
2017-09-12 07:42:07 -0700

    Update and rename binary_decimal.java to binary_decimal1.java

---
+++ binary_decimal1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class binary_decimal1 {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int b=sc.nextInt();
+int d=0;
+int p=0,t;
+while(b!=0)
+{
+	t=b%10;
+	d+=t*Math.pow(2,p);
+	p++;
+	b=b/10;
+}
+System.out.println(d);
+	}
+}
