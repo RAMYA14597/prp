1859378410e2e9007eddd7dbea6ae0c009a7b915
RAMYA14597 <ramyaerusan@gmail.com>
2017-10-08 08:48:21 -0700

    Create char_print.java

---
+++ char_print.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class char_print {
+public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+char[] s="abcdefghijklmnopqrstuvwxyz".toCharArray();
+int i;
+if(a<=26)
+{
+System.out.println(Character.toString(s[a-1]));
+}
+else
+	{
+	System.out.println("null");
+	}
+	}
+
+}
+
