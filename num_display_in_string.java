59cb6dbab3a188d2b89958fa1d47c643789f121e
RAMYA14597 <ramyaerusan@gmail.com>
2018-12-15 23:25:47 +0530

    Create num_display_in_string.java

---
+++ num_display_in_string.java
@@ -0,0 +1,25 @@
+package guvi_prgms;
+
+import java.util.Scanner;
+
+public class num_display_in_string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner sc=new Scanner (System.in);
+String s1=sc.next();
+char ch;int i;
+String s2="";
+for(i=0;i<s1.length();i++)
+{
+	ch=s1.charAt(i);
+	if(Character.isDigit(ch))
+	{
+		s2=s2+ch;
+	}
+
+}
+System.out.println("The numbers in the given string is: "+s2);
+	}
+
+}
